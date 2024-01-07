9b9b0b9586fbb8c385c85369af007a5f3eea0938
Dương Ngọc Thái <dntthai16102003@gmail.com>
2024-01-07 18:29:36 +0700

    update

--- backend/oauth2.py
+++ backend/oauth2.py
@@ -8,7 +8,7 @@ import validations
 
 SECRET_KEY = "157457c9e71d92a8d5286aeae454af18ba72fdb744b6d0569632f8adc2a28a74"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 3000
 
 
 def create_access_token(data: dict):
